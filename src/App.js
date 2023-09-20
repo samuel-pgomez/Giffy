f65f5137a9f675578bbb3361ac0415f8c110c182
Samuel Gomez <133193102+samuel-pgomez@users.noreply.github.com>
2023-09-19 19:23:24 -0600

    Cambio de clase de estilos

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <section className="App-content">
        
-      </header>
+      </section>
     </div>
   );
 }
