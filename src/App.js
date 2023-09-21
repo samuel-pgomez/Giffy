03ba9e5e266bf310040fb1fe603bcd891e5d9e8f
Samuel Gomez <133193102+samuel-pgomez@users.noreply.github.com>
2023-09-20 23:14:37 -0600

    Actualizacion de ListOFGits.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 //Importacion de Hook useState
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import getGifs from "./services/getGifs";
 import ListOfGifs from "./components/ListOfGifs";
 
 //const apiUrl = 'https://api.giphy.com/v1/gifs/search?api_key=Wb03bI088BhPf4BKynXAfCFDF6Bb60Zp&q=panda&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips'
 
 export default function App() {
-  //El useState maneja dos posiciones
-  //El valor del estado
-  //La actualizacion de dicho stado
-  const [gifs, setGifs] = useState([]);
-
-  //useEffect
-  //La funcion
-  //Las dependencias (si no hay solo se ejecuta una sola vez)
-  useEffect(function () {
-    //console.log('actualizando los gifs')
-    //Consultando la API
-    getGifs({ keyword: "chile" }).then((gifs) => setGifs(gifs));
-    //
-  }, []);
-
+  const [keyword, setKeyword] = useState('panda')
   return (
     <div className="App">
       <section className="App-content">
-        <ListOfGifs gifs={gifs} />
+        <button onClick={() => setKeyword('mapache')}>Cambiar keyword</button>
+        <ListOfGifs keyword='ecuador' />
       </section>
     </div>
   );
--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Gif from "./Gif";
+import getGifs from '../services/getGifs'
 
-export default function ListOfGifs({ gifs }) {
+
+export default function ListOfGifs({ keyword }) {
+      //El useState maneja dos posiciones
+  //El valor del estado
+  //La actualizacion de dicho stado
+  const [gifs, setGifs] = useState([]);
+
+  //useEffect
+  //La funcion
+  //La keyword funciona como una dependencia del efecto
+  //Las dependencias (si no hay solo se ejecuta una sola vez)
+  useEffect(function () {
+    //console.log('actualizando los gifs')
+    //Consultando la API
+    getGifs({ keyword }).then((gifs) => setGifs(gifs));
+    //
+  }, []);
+
   return gifs.map(({ id, title, url }) => (
     <Gif 
     id={id} 
