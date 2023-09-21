37aacdabf6ab7364984bb49d13f8af349ee6fc3e
Samuel Gomez <133193102+samuel-pgomez@users.noreply.github.com>
2023-09-20 23:01:45 -0600

    Creacion de ListOfGifs.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import getGifs from "./services/getGifs";
-import Gif from "./components/Gif";
+import ListOfGifs from "./components/ListOfGifs";
 
 //const apiUrl = 'https://api.giphy.com/v1/gifs/search?api_key=Wb03bI088BhPf4BKynXAfCFDF6Bb60Zp&q=panda&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips'
 
@@ -18,27 +18,14 @@ export default function App() {
   useEffect(function () {
     //console.log('actualizando los gifs')
     //Consultando la API
-    getGifs({ keyword: "programing" }).then((gifs) => setGifs(gifs));
+    getGifs({ keyword: "chile" }).then((gifs) => setGifs(gifs));
     //
   }, []);
 
   return (
     <div className="App">
       <section className="App-content">
-        {
-          //Renderiza cada una de las imagenes para que aparezcan todas*/
-          gifs.map((singleGif) => (
-            <Gif
-              //Agregacion de una key unica para cada elemento
-              key={singleGif.id}
-              title={singleGif.title}
-              url={singleGif.url}
-              id={singleGif.id}
-            />
-          )) /**Al hacer click evalua el eevento y cambia de Gif */
-        }
-        {/*<button onClick={() => setGifs(DIFFERENT_GIFS)}>Cambiar Gifs</button>
-         */}
+        <ListOfGifs gifs={gifs} />
       </section>
     </div>
   );
