5909e1dc0ff935bd11e19185b298802ee37f1b34
Samuel Gomez <133193102+samuel-pgomez@users.noreply.github.com>
2023-09-20 23:31:16 -0600

    Agregacion de estilo sencillo

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,8 +1,9 @@
 import React from "react";
+import './Gif.css'
 
 export default function Gif({ title, id, url }) {
   return (
-    <div>
+    <div className="Gif">
       <h4>{title}</h4>
       <small>{id}</small>
       <img alt={title} src={url} />
