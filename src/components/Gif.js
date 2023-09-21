67fc8fae8b96f22ab0395ec0362fcb9c889a0843
Samuel Gomez <133193102+samuel-pgomez@users.noreply.github.com>
2023-09-20 23:38:49 -0600

    Agregacion de Image-button

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -3,10 +3,10 @@ import './Gif.css'
 
 export default function Gif({ title, id, url }) {
   return (
-    <div className="Gif">
+    <a href={`#${id}`} className="Gif">
       <h4>{title}</h4>
       <small>{id}</small>
       <img alt={title} src={url} />
-    </div>
+    </a>
   );
 }
