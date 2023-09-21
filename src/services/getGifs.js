555c2ec9d22ed230a025eb1c287e486ead25fb82
Samuel Gomez <133193102+samuel-pgomez@users.noreply.github.com>
2023-09-20 20:48:27 -0600

    Consultar por parametros en la API

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,8 +1,9 @@
 const apiKey = "Wb03bI088BhPf4BKynXAfCFDF6Bb60Zp";
 
-const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=panda&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
-
-export default function getGifs() {
+//Keyword como parametro
+export default function getGifs({ keyword = "morty" } = {}) {
+  //Para consultar la api con parametros
+  const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
   //Consultando la API
   return fetch(apiUrl)
     .then((res) => res.json()) //Conversion a json
