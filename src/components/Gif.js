1c38820134821c26bbe8f7ca32a82ad1decf0ccc
Samuel Gomez <133193102+samuel-pgomez@users.noreply.github.com>
2023-09-20 23:42:15 -0600

    Actualizacion de estilos

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -5,7 +5,6 @@ export default function Gif({ title, id, url }) {
   return (
     <a href={`#${id}`} className="Gif">
       <h4>{title}</h4>
-      <small>{id}</small>
       <img alt={title} src={url} />
     </a>
   );
