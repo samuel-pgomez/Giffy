b5438edf7d80e8c95efcfd7dfd1b83f232c908ff
Samuel Gomez <133193102+samuel-pgomez@users.noreply.github.com>
2023-09-21 18:02:45 -0600

    Uso de wouter

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React, { useState } from "react";
 import "./App.css";
 import ListOfGifs from "./components/ListOfGifs";
 
+//Utilizando Wouter
+import { Route } from "wouter";
+
 //const apiUrl = 'https://api.giphy.com/v1/gifs/search?api_key=Wb03bI088BhPf4BKynXAfCFDF6Bb60Zp&q=panda&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips'
 
 export default function App() {
@@ -10,8 +13,11 @@ export default function App() {
   return (
     <div className="App">
       <section className="App-content">
-        <button onClick={() => setKeyword('mapache')}>Cambiar keyword</button>
-        <ListOfGifs keyword={keyword} />
+        <h1>App</h1> 
+        <Route 
+        component={ListOfGifs}
+        path="/gif/:keyword"/>
+        
       </section>
     </div>
   );
--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -1,10 +1,10 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Gif from "./Gif";
-import getGifs from '../services/getGifs'
+import getGifs from "../services/getGifs";
 
-
-export default function ListOfGifs({ keyword }) {
-      //El useState maneja dos posiciones
+export default function ListOfGifs({ params }) {
+  const { keyword } = params
+  //El useState maneja dos posiciones
   //El valor del estado
   //La actualizacion de dicho stado
   const [gifs, setGifs] = useState([]);
@@ -13,18 +13,25 @@ export default function ListOfGifs({ keyword }) {
   //La funcion
   //La keyword funciona como una dependencia del efecto
   //Las dependencias (si no hay solo se ejecuta una sola vez)
-  useEffect(function () {
-    //console.log('actualizando los gifs')
-    //Consultando la API
-    getGifs({ keyword }).then((gifs) => setGifs(gifs));
-    //
-  }, [keyword]);
+  useEffect(
+    function () {
+      //console.log('actualizando los gifs')
+      //Consultando la API
+      getGifs({ keyword }).then((gifs) => setGifs(gifs));
+      //
+    },
+    [keyword]
+  );
 
-  return gifs.map(({ id, title, url }) => (
-    <Gif 
-    id={id} 
-    key={id} 
-    title={title} 
-    url={url} />
-  ));
+  return (
+    <div>
+      {gifs.map(({ id, title, url }) => (
+        <Gif id={id}
+         key={id}
+         title={title} 
+         url={url} />
+      ))}
+      ;
+    </div>
+  );
 }
